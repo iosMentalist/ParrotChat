7a779508ed1d16194c422efd3108fbb88f8a57c4
“Shady <shady.kahaleh@gmail.com>
2020-08-19 10:58:07 +0300

    initial commit

---
+++ ParrotChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParrotChat
+//
+//  Created by Shady Kahaleh on 8/18/20.
+//  Copyright © 2020 Shady Kahaleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+#  Parrot Chat
+Parrot chat is a refactored version of my project of  `Chat Bot`  following TDD and solid principles.
+
