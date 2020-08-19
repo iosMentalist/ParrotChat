ba3ab1150bda770e28347fe498b6bea916686db6
“Shady <shady.kahaleh@gmail.com>
2020-08-19 12:46:09 +0300

    add models

---
+++ ParrotChat/Chat/Chat.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Shady
+//  All rights reserved.
+//
+
+import Foundation
+
+struct Chat  {
+    var chatID : Double?
+    var user : User?
+    var messages : [Message]?
+    var date : Date?
+}
---
+++ ParrotChat/Message/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Shady
+//  All rights reserved.
+//  
+
+import Foundation
+
+struct Message {
+    var body : String
+    var date : Date
+    var isMyMessage : Bool
+}
