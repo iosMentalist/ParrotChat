86a89958a1e6ba15117349c6b80aa042673cd266
“Shady <shady.kahaleh@gmail.com>
2020-08-19 16:31:12 +0300

    test save user

---
+++ ParrotChatTests/UserStoringTests.swift
@@ -0,0 +1,44 @@
+//
+//  Created by Shady
+//  All rights reserved.
+//
+
+import XCTest
+@testable import ParrotChat
+
+class UserStore{
+
+    var receivedInvocatins = 0
+
+    func save(user:User){
+        receivedInvocatins += 1
+    }
+}
+class ParrotChatTests: XCTestCase {
+
+    func test_init_doesntInvokeStoreWhenCreated(){
+
+        let sut = makeSUT()
+        XCTAssertEqual(sut.receivedInvocatins, 0)
+    }
+
+    func test_save_withError(){
+        let sut = makeSUT()
+
+        sut.save(user:anyUser())
+
+       XCTAssertEqual(sut.receivedInvocatins, 1)
+    }
+
+    //HELERPS
+    func makeSUT() -> UserStore{
+        let store = UserStore()
+        return store
+    }
+
+    func anyUser() -> User{
+        return User(name: "user name", imageName: "image", lastMessage: Message(body: "body", date: Date(), isMyMessage: true))
+    }
+
+
+}
