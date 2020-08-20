d76058d48bafa4c56468cfafa2f164340509bf75
“Shady <shady.kahaleh@gmail.com>
2020-08-20 13:06:05 +0300

    create new helpers files

--- ParrotChatTests/Helper/CommonHelpers.swift
+++ ParrotChatTests/Helper/CommonHelpers.swift
@@ -4,3 +4,14 @@
 //  
 
 import Foundation
+import ParrotChat
+
+
+//helper function that sends back the same user for model and local boundaires
+func anyUser() -> (model:User,local:LocalUser){
+    let model =  User(name: "user name", imageName: "image", lastMessage: Message(body: "body", date: Date(), isMyMessage: true))
+
+    let local =  LocalUser(name: "user name", imageName: "image", lastMessage: LocalMessage(body: "body", date: Date(), isMyMessage: true))
+
+    return(model,local)
+}
