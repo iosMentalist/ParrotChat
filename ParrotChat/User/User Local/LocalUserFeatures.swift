0c9703e36a7604a60272db54aa7192dd027f1bf2
“Shady <shady.kahaleh@gmail.com>
2020-08-20 23:55:12 +0300

    refactor : combine all feature on one file

---
+++ ParrotChat/User/User Local/LocalUserFeatures.swift
@@ -0,0 +1,66 @@
+//
+//  Created by Shady
+//  All rights reserved.
+//  
+
+import Foundation
+
+public class LocalUserFeatures {
+
+    let store : UserStore
+
+    public init(_ store:UserStore){
+        self.store = store
+    }
+}
+
+extension LocalUserFeatures : UserSaver {
+    public func save(user:User,completion:@escaping(InsertionResult)->Void){
+
+        store.insert(user: user.toLocal()){[weak self] result in
+            guard self != nil else { return }
+
+
+            switch result {
+            case .success(()):
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+extension LocalUserFeatures : UserRetriever {
+    public func retrieveAllUsers(completion: @escaping RetrieveCompletion) {
+         store.retrieveAllUsers { [weak self] result in
+             guard self != nil else {return}
+
+             switch result {
+             case .failure(let error):
+                 completion(.failure(error))
+             case .success(let users):
+                 completion(.success(users))
+
+             }
+         }
+     }
+}
+
+extension LocalUserFeatures : UserDeleter {
+     public func delete(user:User,completion:@escaping(DeletionResult)->Void){
+
+        store.delete(user: user.toLocal()){[weak self] result in
+            guard self != nil else { return }
+
+
+            switch result {
+            case .success(()):
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+}
