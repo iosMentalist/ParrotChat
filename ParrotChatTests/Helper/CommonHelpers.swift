38c1d2dba0d5084298fff9e41840ee4ffdb62064
“Shady <shady.kahaleh@gmail.com>
2020-08-23 00:54:00 +0300

    fix : test cases after refactoring models

--- ParrotChatTests/Helper/CommonHelpers.swift
+++ ParrotChatTests/Helper/CommonHelpers.swift
@@ -9,9 +9,9 @@ import ParrotChat
 
 //helper function that sends back the same user for model and local boundaires
 func anyUser() -> (model:User,local:LocalUser){
-    let model =  User(name: "user name", imageName: "image", lastMessage: Message(body: "body", date: Date(), isMyMessage: true))
+    let model =  User(name: "user name", imageName: "image", chat: nil)
 
-    let local =  LocalUser(name: "user name", imageName: "image", lastMessage: LocalMessage(body: "body", date: Date(), isMyMessage: true))
+    let local =  LocalUser(name: "user name", imageName: "image", chat: nil)
 
     return(model,local)
 }
