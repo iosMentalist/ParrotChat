d54d0c438e08e31fb9703d7da1bc39aa8de42780
“Shady <shady.kahaleh@gmail.com>
2020-08-23 23:03:33 +0300

    refactor : decouple the data setting from the view

--- ParrotChat/Services/UserViewComposer.swift
+++ ParrotChat/Services/UserViewComposer.swift
@@ -10,7 +10,39 @@ struct UserViewComposer {
         let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "UsersViewController") as! UsersViewController
         let store = try! CoreDataStore(storeName: "Store.sqlite")
         let userFeautres  = LocalUserFeatures(store)
-        vc.userFeature = userFeautres
+        setupModel(with: userFeautres, for: vc)
         return vc
     }
+
+    //MARK: Helpers methods
+    static func setupModel(with userFeature : LocalUserFeatures, for vc:UsersViewController){
+        userFeature.retrieveAllUsers(completion: { result in
+            switch result {
+            case.success(let localUsers) :
+                localUsers.isEmpty ? generateUsers(userFeature,vc: vc) :         setupViewControllerModel(for: vc, with: localUsers)
+
+            case.failure(let error):
+                debugPrint("error in retriveing user from db \(error)")
+            }
+        })
+    }
+
+    static private func generateUsers(_ userFeature : LocalUserFeatures, vc:UsersViewController){
+        let users = UserGenerator.generateUsers()
+        for user in users{
+            userFeature.save(user: user, completion: {(result) in
+                switch result {
+                case.success :
+                    debugPrint("success")
+                case.failure(let error):
+                    debugPrint("error in save from db \(error)")
+                }
+            })
+        }
+        setupViewControllerModel(for: vc, with: users.map{$0.local})
+    }
+    static private func setupViewControllerModel(for vc:UsersViewController,with users:[LocalUser]){
+        vc.model = users.map{$0.model}
+    }
+
 }
--- ParrotChat/Views/UI/UsersViewController.swift
+++ ParrotChat/Views/UI/UsersViewController.swift
@@ -8,52 +8,14 @@ import UIKit
 class UsersViewController: UIViewController {
 
     @IBOutlet var tableUsersController: TableUserController!
-
-    var userFeature : LocalUserFeatures?{
-        didSet{
-            setupData()
+    var model : [User]? {
+        willSet{
+            tableUsersController.model = newValue!
         }
     }
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupInterface()
-//        NotificationCenterHelper.listenMessageAdded(observer: self, selector: #selector(refreshView))
-    }
-
-    func setupData(){
-        userFeature?.retrieveAllUsers(completion: { [weak self] (result) in
-            guard let self = self else {return}
-            switch result {
-            case.success(let users) :
-                users.isEmpty ?  self.generateUsers() : self.setupModel(users)
-            case.failure(let error):
-                debugPrint("error in retriveing user from db \(error)")
-            }
-        })
-    }
-
-    private func setupModel(_ users:[LocalUser]){
-        self.tableUsersController.model = users.map{$0.model}
-    }
-
-    private func generateUsers(){
-        let users = UserGenerator.generateUsers()
-        for user in users{
-            userFeature?.save(user: user, completion: { [weak self](result) in
-                guard let self = self else {return}
-                switch result {
-                case.success :
-                    self.setupModel(users.map{$0.local})
-                    debugPrint("save")
-                case.failure(let error):
-                    debugPrint("error in save from db \(error)")
-                }
-
-            })
-        }
-
     }
 
 
