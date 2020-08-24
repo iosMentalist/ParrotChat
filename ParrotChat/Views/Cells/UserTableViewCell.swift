ae0a002196c374f366cf07af12efabf09f4c511c
“Shady <shady.kahaleh@gmail.com>
2020-08-24 14:32:37 +0300

    add new label date for readability

--- ParrotChat/Views/Cells/UserTableViewCell.swift
+++ ParrotChat/Views/Cells/UserTableViewCell.swift
@@ -9,6 +9,7 @@ class UserTableViewCell: UITableViewCell {
     
     @IBOutlet weak var lblUserName: UILabel!
     @IBOutlet weak var lblLastMsg: UILabel!
+    @IBOutlet weak var lblDate: UILabel!
     @IBOutlet weak var imgProfile: UIImageView!
     
     override func awakeFromNib() {
@@ -18,6 +19,11 @@ class UserTableViewCell: UITableViewCell {
         imgProfile.layer.borderColor = UIColor.white.cgColor
         imgProfile.layer.cornerRadius = imgProfile.frame.size.width / 2
         imgProfile.clipsToBounds = true
+
+        lblLastMsg.textColor = .lightGray
+        lblLastMsg.font = UIFont.systemFont(ofSize: 12)
+        lblDate.textColor = .lightGray
+        lblDate.font = UIFont.systemFont(ofSize: 12)
     }
 
 }
--- ParrotChat/Views/Controllers/TableUserController.swift
+++ ParrotChat/Views/Controllers/TableUserController.swift
@@ -5,12 +5,12 @@
 
 import UIKit
 class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
-    
+
     var model = [User]()
     var viewModel = [User]()
     var parentViewController : UIViewController!
     var userFeautres : LocalUserFeatures?
-    
+
     @IBOutlet var tableView : UITableView!{
         didSet{
             tableView.rowHeight = UITableView.automaticDimension
@@ -19,19 +19,19 @@ class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
             tableView.dataSource = self
         }
     }
-    
+
     func setup(parentViewController:UIViewController, model:[User],userFeautres : LocalUserFeatures){
         self.parentViewController = parentViewController
         self.userFeautres = userFeautres
         updateModel(model: model)
         NotificationCenterHelper.listenMessageAdded(observer: self, selector: #selector(userUpdated))
     }
-    
+
     private func updateModel(model:[User]){
         self.model = model
         self.setupViewModelArray()
     }
-    
+
     @objc private func userUpdated(){
         userFeautres?.retrieveAllUsers(){ result in
             switch result {
@@ -42,7 +42,7 @@ class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
                 debugPrint("error in retriveing user from db \(error)")
             }
         }
-        
+
     }
     private func setupViewModelArray(){
         viewModel.removeAll()
@@ -52,7 +52,7 @@ class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
         viewModel.append(contentsOf: usersWithChats)
         viewModel.append(contentsOf: usersWithNoChats)
     }
-    
+
     //MARK: Table View
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -60,34 +60,35 @@ class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = viewModel[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell") as! UserTableViewCell
         configureCell(cell: cell, user: item)
         return cell
     }
-    
+
     private func configureCell(cell:UserTableViewCell,user item:User){
         cell.lblUserName?.text = item.name
         if let lastmsg = item.chat?.messages.last{
             cell.lblLastMsg.isHidden = false
-            cell.lblLastMsg?.text = "\(lastmsg.body)\n\(lastmsg.date.toString())"
-            cell.lblLastMsg.textColor = .lightGray
-            cell.lblLastMsg.font = UIFont.systemFont(ofSize: 12)
+            cell.lblDate.isHidden = false
+            cell.lblLastMsg?.text = "\(lastmsg.body)"
+            cell.lblDate.text = "\(lastmsg.date.toString())"
         }
         else{
             cell.lblLastMsg.isHidden = true
+            cell.lblDate.isHidden = true
         }
         cell.imgProfile?.image = UIImage(named: item.imageName)
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var user = viewModel[indexPath.row]
         user.chat = (user.chat == nil) ? Chat(messages: [], date: Date()) : user.chat
         goToChatDetail(user:user)
     }
-    
+
     //MARK: helpers
     private func goToChatDetail(user:User){
         let vc = ChatViewComposer.create(userFeatures: userFeautres!, user:user)
@@ -98,11 +99,11 @@ class TableUserController :NSObject, UITableViewDelegate, UITableViewDataSource{
 extension Date{
     func toString() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy"
+        dateFormatter.dateFormat = "MMM dd"
         dateFormatter.timeZone = NSTimeZone.local
         dateFormatter.locale = NSLocale(localeIdentifier: "en") as Locale
         let string = dateFormatter.string(from: self)
         return string
     }
-    
+
 }
